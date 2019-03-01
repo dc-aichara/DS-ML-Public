2564f61ba15506d6bf97cf4f85b75015f920dc7c
Dayal Chand Aichara <44660920+dc-aichara@users.noreply.github.com>
2019-03-01 12:02:43 +0900

    Update Bar_Plot_Animation.py

--- Bar_Plot_Animation.py
+++ Bar_Plot_Animation.py
@@ -14,17 +14,17 @@ crypto_cap1.index = crypto_cap1.index.str.upper()  # Covert coins names to upper
 print(crypto_cap1.head())
 
 Writer = animation.writers['ffmpeg']  # Select writer
-writer = Writer(fps=20, metadata=dict(artist='Me'))  # Writer's parameters
+writer = Writer(fps=20, metadata=dict(artist='D C Aichara'))  # Writer's parameters
 
 fig, ax = plt.subplots(figsize=(16, 12))
 
 
 def animate(i):  # Animation function
+    date = rypto_cap1.columns[i]
+    data = (crypto_cap1[date].astype(int).sort_values() / 1000000).astype(int)  # select data range
 
-    data = crypto_cap1[crypto_cap1.columns[i]].astype(int).sort_values() / 1000000  # select data range
-
-    ax.clear()  # clean ax to vaoid overlap
-    data.plot(kind='barh', color='b', width=0.70)  # Horizantal bar plot
+    ax.clear()  # clean ax to avoid overlap
+    data.plot(kind='barh', color='b', width=0.70)  # Horizontal bar plot
     ax.tick_params(labelsize=17)
     ax.set_facecolor('gray')  # Set background color
     ax.patch.set_alpha(0.7)
@@ -34,14 +34,22 @@ def animate(i):  # Animation function
     ax.spines['bottom'].set_color('yellow')  # to set x-axis color
     ax.spines['left'].set_color('yellow')  # to define y-axis color
 
-    plt.title('Top 15 CryptoCurrencies Market Cap-wise', fontsize=20, color='g')
+    plt.title('Top 16 CryptoCurrencies Market Cap-wise', fontsize=20, color='g')
     plt.ylabel('Crypto Currency', fontsize=18, color='b')
     plt.xlabel('Market Capitalization [Millions USD]', fontsize=18, color='b')
 
-    plt.text((data.max() * 0.85), 11, crypto_cap1.columns[i], horizontalalignment='center',\
-             fontsize=24, color='c', bbox=dict(facecolor='k', edgecolor='y'))  # Add date
-    plt.text((data.max() * 0.67), 0.0, 'Created by :\n                  Dayal Chand Aichara\n              \
-    dc.aichara@lastroots.com ', fontsize=16, bbox=dict(facecolor='c', edgecolor='y'))  # add creator's information
+    if 150000 <= data.max() <= 330000: # define x -axis limits
+        plt.xlim(0, 330000)
+        plt.text((330000 * 0.80), 11, date, horizontalalignment='center', \
+                 fontsize=24, color='c', bbox=dict(facecolor='k', edgecolor='y'))  # Add date
+        plt.text(200000, 0.0, 'Created by :\n                  Dayal Chand Aichara\n              \
+            dc.aichara@lastroots.com ', fontsize=16, bbox=dict(facecolor='c', edgecolor='y'))  # add creater information
+    else:
+        plt.xlim(0, 150000)
+        plt.text((150000 * 0.80), 11, date, horizontalalignment='center', \
+                 fontsize=26, color='c', bbox=dict(facecolor='k', edgecolor='y'))  # Add date
+        plt.text(90000, 0.0, 'Created by :\n                  Dayal Chand Aichara\n              \
+            dc.aichara@lastroots.com ', fontsize=16, bbox=dict(facecolor='c', edgecolor='y'))  # add creater information
 
     for i, h in enumerate(data):  # label bars
         ax.text(h - 1, i - 0.15, str(h), color='k', fontweight='bold', fontsize=14)
@@ -50,5 +58,3 @@ def animate(i):  # Animation function
 ani = animation.FuncAnimation(fig, animate, interval=100, frames=575, repeat=False)  # create animation
 ani.save('crypto_market_cap.mp4')  # save Animation
 plt.show()
-
-
