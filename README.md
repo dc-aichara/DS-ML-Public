e17fa6372644db67d8ac6bab7b6f36fd10c69f9a
Dayal Chand Aichara <dc.aichara@lastroots.com>
2019-07-02 11:44:55 +0900

    telegram_user_status.py added

--- README.md
+++ README.md
@@ -15,10 +15,31 @@
 - Get [Lok Sabha elections 2019 results](https://results.eci.gov.in/pc/en/partywise/index.htm) using web scraping technique with requests and BeautifulSoup python packages. 
 ## 4. Bitcoin_messages_telegram.py
 - Get text messages from telegram groups and channels which contain word 'bitcoin' or 'btc'. <br>
+
 Example: 
+
 ```python
 >>> tele_btc_messages.head()
+
 ```
+![](images/tele_text.png)
+## 5. telegram_user_status.py
+- Use to get users who were online in last 24 hours.
+
+Example: 
+```
+>>> cd DS-ML-Public
+>>> python telegram_user_status.py 12345 fe3922d77g6wgwgwyu35g46c9 bitgrit
+Number of active users in last 24 hours is 1530.
+          User               status
+0  Dayal Chand               online
+1       Sameer             recently
+2  Dikesh Shah  2019-07-02 01:13:19
+3       Crypto  2019-07-02 00:47:50
+4        Billy  2019-07-02 01:32:49
+
+```
+
 ![](images/tele_text.png)
 
 ## My Medium Articles
@@ -27,3 +48,4 @@ Example:
 -  [12 Things to know about Jupyter Notebook Markdown](https://medium.com/@dc.aichara/12-things-to-know-about-jupyter-notebook-markdown-3f6cef811707)
 -  [Making Animated Bar Plots of Top 16 CryptoCurrencies (Market Capitalization, Price and Volume)](https://medium.com/@dc.aichara/telegram-channel-data-extraction-users-information-chats-and-specific-messages-and-data-21bb54710fd3)
 -  [Coin Hopping Attack : How did c0ban (First ICO from Japan) deal with it using LWMA ?](https://medium.com/@dc.aichara/coin-hopping-attack-how-did-c0ban-first-ico-from-japan-deal-with-it-using-lwma-9facda2f02b6)
+
---
+++ telegram_user_status.py
@@ -0,0 +1,48 @@
+import sys
+from telethon import TelegramClient, sync
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+def user_status(api_id, api_hash, group_name):
+    """
+    This function can be used to get users who was active in last 24 hours
+    :param api_id: Your API ID
+    :param api_hash: Your API hash
+    :param group_name:  group/channel name
+    :return: pandas DataFrame
+    """
+    client = TelegramClient('session_name', api_id, api_hash).start()
+
+    participants = client.get_participants(group_name)
+
+    time_limit = (datetime.utcnow() - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+
+    user_name = []
+    user_status = []
+
+    if len(participants):
+        for x in participants:
+            try:
+                if str(x.status).startswith('UserStatusRecen'):
+                    user_name.append(x.first_name)
+                    user_status.append('recently')
+                elif str(x.status).startswith('UserStatusOnline'):
+                    user_name.append(x.first_name)
+                    user_status.append('online')
+                elif x.status.was_online.strftime('%Y-%m-%d %H:%M:%S') >= time_limit:
+                    user_name.append(x.first_name)
+                    user_status.append(x.status.was_online.strftime('%Y-%m-%d %H:%M:%S'))
+                else:
+                    pass
+            except:
+                pass
+
+    df = pd.DataFrame({'User':user_name, 'status': user_status})
+    print('Number of active users in last 24 hours is {}.'.format(df.shape[0]))
+    print(df.head(5))
+def main():
+    user_status(int(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
