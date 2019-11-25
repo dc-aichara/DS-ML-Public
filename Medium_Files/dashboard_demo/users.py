2593e9d136c6fb58b55171922eb62bdc2c26eaf4
Dayal Chand Aichara <dc.aichara@lastroots.com>
2019-11-25 10:53:40 +0900

    Dashboard tutorials added

---
+++ Medium_Files/dashboard_demo/users.py
@@ -0,0 +1,11 @@
+
+user_pwd = {'dash': 'dash@123',
+            'dash1': 'dash1@123',
+            }
+user_names = {'dash': ', welcome to the crypto indicators dashboard',
+              'dash1': ', welcome to the crypto indicators dashboard',
+              }
+
+
+def users_info():
+    return user_pwd, user_names
