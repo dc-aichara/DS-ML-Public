f1c82a930730337131763699c6754d42225c1303
dc-aichara <dcaak7@gmail.com>
2020-10-31 14:53:58 +0900

    Add app title, description, and favicon

--- Medium_Files/dashboard_demo/app.py
+++ Medium_Files/dashboard_demo/app.py
@@ -1,6 +1,12 @@
 import dash
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash('Cryptocurrency Indicators Dashboard', external_stylesheets=external_stylesheets)
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+app = dash.Dash(
+    "Cryptocurrency Indicators Dashboard",
+    external_stylesheets=external_stylesheets,
+)
 server = app.server
 app.config.suppress_callback_exceptions = True
-
+app.title = "Cryptocurrency Indicators Dashboard"
+app.description = """A dashboard to display price indicators for Bitcoin, 
+                  Ethereum, Ripple, and Bitcoin-cash."""
